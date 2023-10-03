d4b9cb1b0af37e8610e0427a2459b29c548eda80
Dilshod <46994171+IamDilshod@users.noreply.github.com>
2023-10-03 10:05:29 +0500

    some updat

---
+++ Zaur Tregulov/src/Main5.java
@@ -0,0 +1,11 @@
+public class Main5 {
+	public static void main(String... args) {
+		double price = 10;
+		String model;
+//		if (price > 10)
+//			model = "Smartphone";
+//		else if (price <= 10)
+//			model = "Landline";
+//		System.out.println(model);
+	}
+}
